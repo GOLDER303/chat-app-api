96f947d6716a0a05f102ae2b3e410b29dc2535ff
GOLDER303 <szymonstepien@interia.pl>
2024-02-21 16:20:42 +0100

    Change cors methods

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
