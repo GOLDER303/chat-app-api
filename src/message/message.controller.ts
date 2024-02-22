efa62f0a43307817f5d999a07c2bf2099b0d6ab9
GOLDER303 <szymonstepien@interia.pl>
2024-02-22 13:39:17 +0100

    Change message controller url

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,21 +1,18 @@
-import { Body, Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AccessTokenGuard } from 'src/common/guards/access-token.guard';
 import { ChatOwnerGuard } from 'src/common/guards/chat-owner.guard';
-import { GetMessagesRequestDTO } from './dtos/get-messages-request.dto';
 import { MessageResponseDTO } from './dtos/message-response.dto';
 import { MessageService } from './message.service';
 
-@Controller('messages')
+@Controller('chats/:chatId/messages')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
   @UseGuards(AccessTokenGuard, ChatOwnerGuard)
   @Get('/')
-  async getChatMesseges(
-    @Body() getMessagesRequestDTO: GetMessagesRequestDTO,
+  async getChatMessages(
+    @Param('chatId') chatId: string,
   ): Promise<MessageResponseDTO[]> {
-    return await this.messageService.getChatMessages(
-      getMessagesRequestDTO.chatId,
-    );
+    return await this.messageService.getChatMessages(+chatId);
   }
 }
