05fa09be65f2f6ded5082c57a77fb3461ee60d1b
GOLDER303 <szymonstepien@interia.pl>
2024-02-22 14:45:06 +0100

    Fix prisma 5 breaking changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +14,5 @@ async function bootstrap() {
   });
 
   await app.listen(8080);
-
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
