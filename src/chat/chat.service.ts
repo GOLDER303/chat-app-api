314add6637eb28f375bf3182e7348673a7a83fb5
GOLDER303 <szymonstepien@interia.pl>
2024-04-07 19:28:06 +0200

    Remove a console log

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -166,7 +166,6 @@ export class ChatService {
         const prevChatImageFilePath = join('uploads', prevChatImageFileName);
 
         if (existsSync(prevChatImageFilePath)) {
-          console.log(`removing ${prevChatImageFilePath}`);
           unlinkSync(prevChatImageFilePath);
         }
       }
