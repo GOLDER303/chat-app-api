c96bbe91bdbd651e1bb8a4491af7ed1c9935137e
GOLDER303 <szymonstepien@interia.pl>
2024-04-09 21:50:57 +0200

    Add an endpoint for adding user profile images

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AccessTokenGuard } from 'src/common/guards/access-token.guard';
+import { multerOptions } from 'src/config/multer-options';
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -11,5 +22,19 @@ export class UserController {
   @Get('/')
   async getAllUsers(): Promise<UserDTO[]> {
     return await this.userService.getAllUsers();
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Post('/:userId/image')
+  @UseInterceptors(FileInterceptor('image', multerOptions))
+  async uploadUserImage(
+    @Param('userId') userId: string,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    if (!image) {
+      throw new BadRequestException('You have to provide an image');
+    }
+
+    await this.userService.addUserImage(+userId, image);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { existsSync, unlinkSync } from 'fs';
+import { join } from 'path';
 import { PrismaService } from './../prisma/prisma.service';
 import { UserDTO } from './dto/user.dto';
 
@@ -31,5 +33,33 @@ export class UserService {
     });
 
     return userIds.filter((userId) => !existingUserIds.has(userId));
+  }
+
+  async addUserImage(userId: number, image: Express.Multer.File) {
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where: { id: userId },
+      });
+
+      const prevUserImageFileName = user.userImageFileName;
+
+      if (prevUserImageFileName) {
+        const prevChatImageFilePath = join('uploads', prevUserImageFileName);
+
+        if (existsSync(prevChatImageFilePath)) {
+          unlinkSync(prevChatImageFilePath);
+        }
+      }
+
+      return await this.prisma.chat.update({
+        where: { id: userId },
+        data: {
+          chatImageFileName: image.filename,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new NotFoundException(`User with id: ${userId} does not exist`);
+    }
   }
 }
